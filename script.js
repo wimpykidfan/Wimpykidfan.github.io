06357bc5d1c218bde7c51bbe8d394909d705fc35
Daniel Segarra <109241857+DanielTUW@users.noreply.github.com>
2022-09-15 19:28:10 +0000

    Updates

--- script.js
+++ script.js
@@ -1,8 +1,26 @@
-document.querySelector('.btn').addEventListener('click', function() {
-        let name = prompt('What is your name?');
-        if (name.length) {
-          alert('Thanks, ' + name + ' , we will be in touch. Meanwhile, have a lot of Yogurt!')
-        } else {
-          alert('No name, no Greek Yogurt for you!')
-        }
-      });
+/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
+function myFunction() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
+
+/* ConstructionCurr js*/
+
+var coll = document.getElementsByClassName("collapsible");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.maxHeight){
+      content.style.maxHeight = null;
+    } else {
+      content.style.maxHeight = content.scrollHeight + "px";
+    } 
+  });
+}
